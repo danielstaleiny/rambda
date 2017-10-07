780f1bbb56d72490740c622de7567f338ec452bf
danielstaleiny <daniel.rafaj@gmail.com>
2017-10-07 21:13:28 +0200

    func methods and testing magit

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const replace = require('ramda/replace')
-const _= require('ramda/__')
+const R = require("rambda");
 
-var greet = replace('{name}', R.__, 'Hello, {name}!')
-console.log(greet('Alice'))
-console.log(typeof greet)
+const result = R.compose(R.filter(R.equals(2)), R.map(R.add(1)))({
+  a: 1,
+  b: 2,
+  c: 3
+});
 
+console.log(result);
+
+// add
+const add2 = R.add(2);
+
+console.log(add2(3)); //5
+console.log(R.add(2, 3)); //5
+console.log(R.add(2, add2(2))); //6
+
+const arr = [0, 1, 3, 4, 5];
+const fn = n => n >= 0;
+const fn2 = n => n > 0;
+console.log(R.all(fn, arr)); //true
+console.log(R.all(fn2, arr)); //false
+
+const result2 = R.compose(R.map(x => x * 2), R.filter(x => x > 2))([
+  1,
+  2,
+  3,
+  4,
+  5
+]);
+console.log(result2);
